1fe57035cd17cf9b142215a9a4247557287c5dae
Jeongin Lee <jeonginlee0304@gmail.com>
2022-10-18 23:14:34 -0400

    add router for each page and hyperlinks to the nav bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Radio from './pages/Radio'
+import Search from './pages/Search'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        {/* a route for the home page */}
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="/radio" element={<Radio />} />
+
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
   );
 }
 
