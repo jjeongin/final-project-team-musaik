39681180da62ebb0f11ea372342479b24dd56364
Ryan Rochmanofenna <rr3758@nyu.edu>
2022-10-24 16:54:36 -0400

    added react file for Radio bubbles list

---
+++ front-end/src/pages/Radio/index.jsx
@@ -0,0 +1,17 @@
+import NavBar from '../../NavBar';
+import 'front-end\src\pages\Radio\index.css'
+
+
+
+function Radio() {
+    return (
+        <div className="Radio">
+            <div className="RadioBubbles">
+
+            </div>
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Radio
