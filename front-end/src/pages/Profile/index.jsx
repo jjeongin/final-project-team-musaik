9cef72d293fbbbc642b3bcbaeed9887626c77ca3
Ahmadhcs <aalmesned1@gmail.com>
2022-10-25 14:13:04 -0400

    changed image to make it a prop, changed pfp sizing

--- front-end/src/pages/Profile/index.jsx
+++ front-end/src/pages/Profile/index.jsx
@@ -4,18 +4,19 @@ import UserNumbers from '../../UserNumbers'
 import './styles.css'
 import SearchBar from '../../SearchBar';
 import NavBar from '../../NavBar';
+import avi from "../../img/avi.png";
+
 
 function Profile() {
   return (
     <div className="Profile">
-        <UserAvatar text={"Ahmahcs"} />
+        <UserAvatar text={"Ahmahcs"} image={avi} />
         <UserNumbers followers={300} following={200}/>
 
         <div className='Album-Card'>
           <Albums text={"Favorites"}/>
           <Albums text={"Recently Played"}/>
         </div>
-        <SearchBar />
        <NavBar />
 
     </div>
