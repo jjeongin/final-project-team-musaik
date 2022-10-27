4b88f53bf479a3f9ec32fd8438dbf51307abe06e
Ahmadhcs <aalmesned1@gmail.com>
2022-10-26 21:58:39 -0400

    Added simple following feature, will expand upon once backend is worked on

--- front-end/src/components/UserNumber/UserNumbers.js
+++ front-end/src/components/UserNumber/UserNumbers.js
@@ -1,21 +1,36 @@
 import './UserNumbers.css'
+import React, { useState } from 'react';
 
 
 function UserNumbers({followers, following}){
+
+    const [followerCount, setFollowers] = useState(followers)
+
+    const [followingCount, setFollowing] = useState(following)
+
+    function incrementFollowers(){
+        setFollowers(followers + 1)
+    }
+
+    function incrementFollowing(){
+        setFollowing(following+1)
+    }
+
     return(
+
 
         <div className='User-Card'>
             <div className='Followers-Container'>
-                <h4>{followers}</h4>
+                <h4>{followerCount}</h4>
                 <h4>Followers</h4>
             </div>
 
             <div className='Button-Container'>
-            <button type="button" className='Edit-Button'>Edit</button>
+            <button onClick={incrementFollowers} type="button" className='Edit-Button'>Edit</button>
             </div>
 
             <div className='Following-Container'>
-                <h4>{following}</h4>
+                <h4>{followingCount}</h4>
                 <h4>Following</h4>
             </div>
         </div>
