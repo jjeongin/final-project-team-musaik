5dee660a4302d2365a3e07f35c2eca2504ca6e97
Jeongin Lee <jeonginlee0304@gmail.com>
2022-10-27 11:30:39 -0400

    update login page and make it fit to mobile screen
    redesigned login page based on iPhone SE screen size

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -6,16 +6,10 @@ import apple from '../../img/apple-music-icon.svg'
 function Login() {
   return (
     <div className="Login">
-      <img src={logo} alt="Musaik Logo"/>
-      <div className='login-social'>
-        <div className='login-spotify'>
-          <img src={spotify} alt="Spotify Icon"/>
-          <p>Continue with Spotify</p>
-        </div>
-        <div className='login-apple-music'>
-          <img src={apple} alt="Apple Music Icon"/>
-          <p>Continue with Apple Music</p>
-        </div>
+      <img className="musaik-logo" src={logo} alt="Musaik Logo"/>
+      <div className='login-spotify'>
+        <img src={spotify} alt="Spotify Icon"/>
+        <p>Continue with Spotify</p>
       </div>
     </div>
   );
