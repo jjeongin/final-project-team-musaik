5dfb595ecc0a14225a2a1ed1db2f206ce4b78767
Zain Faruqi <zf698@nyu.edu>
2022-11-01 23:46:00 -0400

    Spotify Oauth and access token in cookie

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,26 @@
 require('dotenv').config({ path: 'config.env' });
 const cors = require('cors');
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const session = require('express-session');
 
 // database setup
 //require('./db');
 //const mongoose = require('mongoose');
 
-// express
-const express = require('express');
+
 const app = express();
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}))
 
 // static files
 const path = require("path");
@@ -25,12 +33,102 @@ app.use(express.urlencoded({ extended: true }));
 //app.use('/api', require('./routes/api'));
 
 
+// spotify api
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'http://localhost:8080/callback'
+});
+
+const scopes = [
+    'user-read-private',
+    'user-read-email',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-recently-played',
+    'user-top-read',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'user-library-read',
+    'user-library-modify',
+    'user-follow-read',
+    'user-follow-modify'
+];
+
+// login
+app.get('/auth', (req, res) => {
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+// callback
+app.get('/callback', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (error) {
+        console.error('Callback Error:', error);
+        res.redirect('/login');
+        return;
+    }
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            const access_token = data.body['access_token'];
+            const refresh_token = data.body['refresh_token'];
+            const expires_in = data.body['expires_in'];
+
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+
+            req.session.access_token = access_token;
+            req.session.refresh_token = refresh_token;
+
+            console.log('access_token:', access_token);
+            console.log('refresh_token:', refresh_token);
+
+            console.log(
+                `Sucessfully retreived access token. Expires in ${expires_in} s.`
+            );
+
+
+            res.redirect("/profile");
+        })
+});
+
+// refresh
+app.get('/refresh', (req, res) => {
+    const refreshToken = req.query.refresh_token;
+    spotifyApi.setRefreshToken(refreshToken);
+    spotifyApi.refreshAccessToken().then(
+        data => {
+            res.json({
+                access_token: data.body['access_token'],
+                expires_in: data.body['expires_in']
+            });
+        }
+    ).catch(
+        err => {
+            console.log(err);
+            res.sendStatus(400);
+        }
+    );
+});
+
+
+
 // for deployment, ignore
 if (process.env.NODE_ENV == 'production') {
     console.log(__dirname);
     app.use(express.static(path.join(__dirname, '../front-end/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../front-end', 'build', 'index.html'));
     });
 } else {
     // development
