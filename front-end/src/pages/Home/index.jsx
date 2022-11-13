e260ef6e6343ea7049f663e63e610e2a50019273
Jeongin Lee <jeonginlee0304@gmail.com>
2022-11-13 04:41:34 -0500

    add recently played songs

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -9,22 +9,30 @@ import {useEffect,useState} from 'react';
 import axios from 'axios';
 
 function Home() {
-
-  const [recs, setRecs] = useState([]);
-
+  // get recently played songs
+  const [songs, setSongs] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:8080/api/rec')
-        .then(res => {
-          console.log(res.data)
-          setRecs([...recs,...res.data])
-        });
-}, []);
+      axios.get('http://localhost:8080/api/get_saved')
+          .then(res => {
+            console.log(res.data)
+            setSongs([...songs,...res.data])
+          });
+  }, []);
+  // get recommendations
+  const [recs, setRecs] = useState([]);
+  useEffect(() => {
+      axios.get('http://localhost:8080/api/rec')
+          .then(res => {
+            console.log(res.data)
+            setRecs([...recs,...res.data])
+          });
+  }, []);
   return (
     <div className="Home">
       <SearchBar />
       <div className="home-content">
         <RadioMatch img1={placeHolder} img2={placeHolder} />
-        <Albums text={"Recently Played"} image={placeHolder}/>
+        <Albums text={"Recently Played Sessions"} image1={songs[1]} image2={songs[2]} image3={songs[0]}/>
         <Albums text={"Recommendations Based on Your Taste"} image1={recs[4]}  image2={recs[0]} image3={recs[2]}/>
       </div>
       <NavBar /> 
