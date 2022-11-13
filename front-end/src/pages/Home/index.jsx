05d30a1745a6b460f4e51273fc47291d42cb1faa
ChineduNnorom <cjn310@nyu.edu>
2022-11-12 22:41:50 -0500

    pinning playlists

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -11,6 +11,17 @@ import axios from 'axios';
 function Home() {
 
   const [recs, setRecs] = useState([]);
+  const [lists, setLists] = useState([]);
+
+
+
+  useEffect(() => {
+    axios.get('/playlists/pin-playlists')
+      .then(res => {
+        console.log(res.data.playlists);
+        setLists([...lists,...res.data.playlists]);
+      })
+  }, []);
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/rec')
@@ -19,12 +30,18 @@ function Home() {
           setRecs([...recs,...res.data])
         });
 }, []);
+
+
+// {unreadMessages.length > 0 &&        <h2>          You have {unreadMessages.length} unread messages.        </h2>      }
+
+
+
   return (
     <div className="Home">
       <SearchBar />
       <div className="home-content">
         <RadioMatch img1={placeHolder} img2={placeHolder} />
-        <Albums text={"Recently Played"} image={placeHolder}/>
+        <Albums text={"Playlists"} image1={lists[1]}  image2={lists[0]} image3={lists[2]}/>
         <Albums text={"Recommendations Based on Your Taste"} image1={recs[4]}  image2={recs[0]} image3={recs[2]}/>
       </div>
       <NavBar /> 
