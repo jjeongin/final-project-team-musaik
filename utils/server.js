811e48ac1c1dc4ba99ff124e1d3333b20ed58a7f
ChineduNnorom <cjn310@nyu.edu>
2022-12-01 17:49:33 -0500

    import data db function

--- utils/server.js
+++ utils/server.js
@@ -25,29 +25,29 @@ app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
 
 
-connectDB();
+// connectDB();
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use("/api/v1/posts", postRoutes);
-
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+// app.use(express.json());
+// app.use("/api/v1/posts", postRoutes);
 
 
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
 
 
 
-connectDB();
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/v1/posts", postRoutes);
 
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+// connectDB();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use("/api/v1/posts", postRoutes);
+
+
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log('Server running on port ${PORT}'));
