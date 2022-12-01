cc7a48c9ef6a0cf5a93d6c75dbf51c2b3dc72de9
ChineduNnorom <cjn310@nyu.edu>
2022-12-01 17:27:17 -0500

    DB Server.js file

--- utils/server.js
+++ utils/server.js
@@ -2,3 +2,11 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config.db");
 const postRoutes = require("./routes/postROutes");
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1/posts", postRoutes);
