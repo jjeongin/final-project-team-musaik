41c00cd96a6298b02f4ce44aa11450a724838031
ChineduNnorom <cjn310@nyu.edu>
2022-12-01 17:34:00 -0500

    complete server.js file DB

--- utils/server.js
+++ utils/server.js
@@ -10,3 +10,44 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/v1/posts", postRoutes);
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+
+
+
+
+
+
+
+
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1/posts", postRoutes);
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+
+
+
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1/posts", postRoutes);
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
